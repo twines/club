6dc2888ffd288b1f933e2a9ba678e7ccf93c5e2c
1355081829@qq.com <1355081829aA>
2019-07-21 17:26:21 +0800

    生成xml的网站地图

--- app/Console/Commands/SiteMap.php
+++ app/Console/Commands/SiteMap.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Category;
+use App\Models\TopicVideo;
 use Illuminate\Console\Command;
 
 class SiteMap extends Command
@@ -38,5 +40,44 @@ class SiteMap extends Command
     public function handle()
     {
         //
+        $categoryList = Category::all();
+        $xml = '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        foreach ($categoryList as $category) {
+            $xml .= '<sitemap>';
+            $xml .= '<loc>';
+            $xml .= url('/sitemap', ['id' => $category->id]) . '.xml';
+            $xml .= '</loc>';
+            $xml .= '<lastmod>';
+            $xml .= $category->updated_at;
+            $xml .= '</lastmod>';
+            $xml .= '</sitemap>';
+        }
+        $xml .= '</sitemapindex>';
+        $siteMap = public_path('/') . '/sitemap.xml';
+        @unlink($siteMap);
+        file_put_contents($siteMap, $xml, FILE_APPEND);
+        foreach ($categoryList as $category) {
+            $siteMap = public_path('/') . "sitemap/{$category->id}.xml";
+            @unlink($siteMap);
+            $videoTopicList = TopicVideo::where('category_id', $category->id)->get();
+            $xml = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">';
+            if ($videoTopicList) {
+                foreach ($videoTopicList as $video) {
+                    $xml .= '<url>';
+                    $xml .= '<loc>';
+                    $xml .= url('/', ['id' => $category->id]) . '.html';
+                    $xml .= '</loc>';
+                    $xml .= '<priority>0.8</priority>';
+                    $xml .= '<lastmod>';
+                    $xml .= $video->updated_at;
+                    $xml .= '</lastmod>';
+                    $xml .= '<changefreq>daily</changefreq>';
+                    $xml .= '</url>';
+                }
+            }
+            $xml .= '</urlset>';
+            file_put_contents($siteMap, $xml, FILE_APPEND);
+        }
+
     }
 }
